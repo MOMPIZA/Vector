553e80da4b2a7a46ccf723f78c1fb912008a70e0
mompiza <khobensky@gmail.com>
2020-01-22 22:54:50 +0700

    Signed-off-by: mompiza <khobensky@gmail.com>

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Location = new System.Drawing.Point(316, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(617, 370);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(884, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(745, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 149);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 193);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(78, 235);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(78, 272);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(196, 133);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(196, 170);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 8;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(196, 221);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 9;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(196, 272);
+            this.trackBar4.Maximum = 100;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(104, 45);
+            this.trackBar4.TabIndex = 10;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(987, 530);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Red;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,190 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Graphics G; // Объект графики
+        PointF[] Arr = new PointF[4]; // Исходный массив точек
+        PictureBox[] pic1 = new PictureBox[100]; 
+        int Fuctorial(int n) // Функция вычисления факториала
+        {
+            int res = 1;
+            for (int i = 1; i <= n; i++)
+                res *= i;
+            return res;
+        }
+        float polinom(int i, int n, float t)// Функция вычисления полинома Бернштейна
+        {
+            return (Fuctorial(n) / (Fuctorial(i) * Fuctorial(n - i))) * (float)Math.Pow(t, i) * (float)Math.Pow(1 - t, n - i);
+        }
+        void Draw()// Функция рисования кривой
+        {
+            //pictureBox2.Image = null;
+           // G.Clear(Color.White);
+            G.Clear(pictureBox2.BackColor);
+            int j = 0;
+            float step = 0.01f;// Возьмем шаг 0.01 для большей точности
+
+            PointF[] result = new PointF[101];//Конечный массив точек кривой
+            for (float t = 0; t < 1; t += step)
+            {
+                float ytmp = 0;
+                float xtmp = 0;
+                for (int i = 0; i < Arr.Length; i++)
+                { // проходим по каждой точке
+                    float b = polinom(i, Arr.Length - 1, t); // вычисляем наш полином Бернштейна
+                    xtmp += Arr[i].X * b; // записываем и прибавляем результат
+                    ytmp += Arr[i].Y * b;
+                }
+                result[j] = new PointF(xtmp, ytmp);
+                j++;
+
+            }
+            G.DrawLines(new Pen(Color.Blue), result);// Рисуем полученную кривую Безье
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+
+            PictureBox pic2 = new PictureBox();
+            this.Controls.Add(pic2);
+            pic2.Show();
+            //pic2 = new PictureBox();
+            pic2.Left = Cursor.Position.X;
+            pic2.Top = Cursor.Position.Y;
+            pic2.Height = 100;
+            pic2.Width = 100;
+            pic2.BackColor = Color.Red;
+            G = pic2.CreateGraphics();
+            G.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Draw();
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+           
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PictureBox pic2 = new PictureBox();
+            this.Controls.Add(pic2);
+            pic2.Show();
+            //pic2 = new PictureBox();
+           // Point Control.PointToClient(Point point);
+            pic2.Left = Cursor.Position.X;
+            pic2.Top = Cursor.Position.Y;
+
+            pic2.Height = 100;
+            pic2.Width = 100;
+            pic2.BackColor = Color.Red;
+            G = pic2.CreateGraphics();
+            G.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Draw();
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+          
+            Arr[0] = new PointF(50, 0);
+            int x1 = trackBar1.Value;
+            int y1 = trackBar2.Value;
+            int x2 = trackBar3.Value;
+            int y2 = trackBar4.Value;
+            Arr[1] = new PointF(x1, y1);
+            Arr[2] = new PointF(x2, y2);
+            Arr[3] = new PointF(0, 50);
+            G = pictureBox2.CreateGraphics();
+           // G = Graphics.FromHwnd(pictureBox2.Handle);
+            G.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Draw();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            Arr[0] = new PointF(50, 0);
+            int x1 = trackBar1.Value;
+            int y1 = trackBar2.Value;
+            int x2 = trackBar3.Value;
+            int y2 = trackBar4.Value;
+            Arr[1] = new PointF(x1, y1);
+            Arr[2] = new PointF(x2, y2);
+            Arr[3] = new PointF(0, 50);
+            G = pictureBox2.CreateGraphics();
+            // G = Graphics.FromHwnd(pictureBox2.Handle);
+            G.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Draw();
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            Arr[0] = new PointF(50, 0);
+            int x1 = trackBar1.Value;
+            int y1 = trackBar2.Value;
+            int x2 = trackBar3.Value;
+            int y2 = trackBar4.Value;
+            Arr[1] = new PointF(x1, y1);
+            Arr[2] = new PointF(x2, y2);
+            Arr[3] = new PointF(0, 50);
+            G = pictureBox2.CreateGraphics();
+            // G = Graphics.FromHwnd(pictureBox2.Handle);
+            G.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Draw();
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            Arr[0] = new PointF(50, 0);
+            int x1 = trackBar1.Value;
+            int y1 = trackBar2.Value;
+            int x2 = trackBar3.Value;
+            int y2 = trackBar4.Value;
+            Arr[1] = new PointF(x1, y1);
+            Arr[2] = new PointF(x2, y2);
+            Arr[3] = new PointF(0, 50);
+            G = pictureBox2.CreateGraphics();
+            // G = Graphics.FromHwnd(pictureBox2.Handle);
+            G.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Draw();
+        }
+
+        private void pictureBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            PictureBox pic2 = new PictureBox();
+            this.Controls.Add(pic2);
+            pic2.Show();
+            //pic2 = new PictureBox();
+            // Point Control.PointToClient(Point point);
+            var location = e.Location;
+            pic2.Left = location.X ;
+            pic2.Top = location.Y ;
+           
+
+            pic2.Height = 100;
+            pic2.Width = 100;
+            pic2.BackColor = Color.Red;
+            Arr[0] = new PointF(50, 0);
+            int x1 = trackBar1.Value;
+            int y1 = trackBar2.Value;
+            int x2 = trackBar3.Value;
+            int y2 = trackBar4.Value;
+            Arr[1] = new PointF(x1, y1);
+            Arr[2] = new PointF(x2, y2);
+            Arr[3] = new PointF(0, 50);
+            G = pic2.CreateGraphics();
+            G.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Draw();
+
+        }
     }
 }
